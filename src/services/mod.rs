bf46e8b305f1a7944ac9286e386a7bfe957ef3f9
Frode Egeland <egeland@gmail.com>
2026-06-25 22:00:10 +1000

    Delete unused abstraction

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,9 +1,7 @@
 pub mod cache_service;
 pub mod fallback_service;
 pub mod stats_service;
-pub mod tile_service;
 
 pub use cache_service::*;
 pub use fallback_service::*;
 pub use stats_service::*;
-pub use tile_service::*;
